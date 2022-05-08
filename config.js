4eaa62fc77a44e2fbaa90421fbbee7a3d5f61d0a
Dmitri Grabov <d.grabov@gmail.com>
2022-05-08 21:11:12 +0100

    add config

---
+++ config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: 'example',
+  display_title: 'example',
+  SERVER_ROOT: 'http://localhost:4040',
+  EVENTS_EXT: '/api/timemap_data/export_events/deeprows',
+  ASSOCIATIONS_EXT: '/api/timemap_data/export_associations/deeprows',
+  SOURCES_EXT: '/api/timemap_data/export_sources/deepids',
+  SITES_EXT: '',
+  SHAPES_EXT: '',
+  DATE_FMT: 'DD/MM/YYYY',
+  TIME_FMT: 'hh:mm',
+  MAPBOX_TOKEN: 'pk.eyJ1IjoiZG1pdHJpZyIsImEiOiJjbDJjYnpreTIwY28yM2dudWQ0NGlxejl6In0.qzCHDJ0dRBn6BXy4fkqTIg',
+  store: {
+    app: {
+      map: {
+        anchor: [46.444431, 32.059769],
+        startZoom: 6
+      },
+      timeline: {
+        range: [new Date(2022, 1, 23, 12), new Date(2022, 4, 23, 12)],
+        rangeLimits: [new Date(2022, 1, 1, 1), new Date()]
+      }
+    },
+    ui: {
+      card: {
+        layout: {
+          template: 'sourced'
+        }
+      }
+    },
+    features: {
+      COLOR_BY_ASSOCIATION: true,
+      USE_ASSOCIATIONS: true,
+      USE_FULLSCREEN: true,
+      USE_SOURCES: true,
+      USE_COVER: false,
+      GRAPH_NONLOCATED: false,
+      HIGHLIGHT_GROUPS: false
+    }
+  }
+}
