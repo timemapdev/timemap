8f6e3989dfe375f41aa692d5b3bb452a2fa8ea35
Dmitri Grabov <d.grabov@gmail.com>
2023-10-18 12:41:14 +0100

    add source map

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build'
+      outDir: 'build',
+      sourcemap: true,
     },
     plugins: [react()],
     server: {
